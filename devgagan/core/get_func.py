33f87c9018e8f0f332d21746d76799538ca36e0c
Azad <azadmalakiya011@gmail.com>
2026-09-23 11:27:36 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -246,6 +246,8 @@ async def upload_media(sender, target_chat_id, file, caption, edit, topic_id):
                     document=file,
                     caption=caption,
                     thumb=thumb_path,
+                    has_spoiler=True,
+                    supports_streaming=True,
                     reply_to_message_id=topic_id,
                     parse_mode=ParseMode.MARKDOWN,
                     progress=progress_bar,
